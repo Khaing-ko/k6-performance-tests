656a761f3200d2289490d19188648c79754fe056
naymintun <naymintun9116@gmail.com>
2025-06-13 15:59:31 +0630

    Document And Other

--- test-scenarios/GetAllEmpForFilter.js
+++ test-scenarios/GetAllEmpForFilter.js
@@ -1,54 +1,23 @@
 import http from 'k6/http';
+import { Configuration } from '.././enviroment/Configuration.js';
 import { check } from 'k6';
 import { sleep } from 'k6';
-import { baseUrl } from '../enviroment/Configuration.js';
-import { getTokenResponse,getCommonHeaders } from '../enviroment/Authentication.js';
+import { baseUrl } from '.././enviroment/Header.js';
+import { getTokenResponse } from './Token.js';
+import { getCommonHeaders } from '.././enviroment/Header.js';
 
-export const options = {
-    stages: [
-  { duration: '1m', target: 100 }, 
-  { duration: '1m', target: 200 },
-  {duration : '30s', target:0}
-],
-
-  thresholds: {
-    http_req_duration: ['p(95)<1500'], 
-    http_req_failed: ['rate<0.01'],   
-  }
-};
+export const options = Configuration;
 
 export default function () {
+  
+  const bearerToken = getTokenResponse();
+let headers = {
+    headers: getCommonHeaders(bearerToken)
+  };
 
-  const token = getTokenResponse();
-  const commonHeaders = getCommonHeaders(token);
-
-    const employeeFilterPayload = {
-      gridState: {
-        page: 1,
-        pageSize: 4000
-      },
-      data: {
-        Type: "true",
-        View_Type: 0,
-        Include_Resign: false,
-        Include_Active: true,
-        Include_Inactive: false,
-        Include_ResignMonthOnly: false,
-        Resign_CustomPeriod: false,
-        Fromdate: null,
-        Todate: null,
-        Resign_DateRange: false,
-        StartDate: null,
-        EndDate: null,
-        Exclude_Resign_DateRange: false,
-        excludeStartDate: null,
-        excludeEndDate: null
-      }
-    };
-
-    const res=http.post(`${baseUrl}/employeesetupweb/GetAllEmployeeForFilter`, JSON.stringify(employeeFilterPayload), {
-      headers: commonHeaders
-    });
+    const FilterAndTreeViewPayload = JSON.parse(open('./enviroment/FilterAndTreeViewPayload.json'));
+    const FirstUrl = `${baseUrl}/employeesetupweb/GetAllEmployeeForFilter`;
+    const res=http.post(FirstUrl, FilterAndTreeViewPayload, headers);
     check(res, {
       'GetAllEmployeeForFilter status is 200': (r) => r.status === 200,
       'GetAllEmployeeForFilter response time < 500ms': (r) => r.timings.duration < 500
--- test-scenarios/GetEmpTreeView.js
+++ test-scenarios/GetEmpTreeView.js
@@ -1,53 +1,23 @@
 import http from 'k6/http';
+import { Configuration } from '.././enviroment/Configuration.js';
 import { check } from 'k6';
 import { sleep } from 'k6';
-import { baseUrl } from '../enviroment/Configuration.js';
-import { getTokenResponse, getCommonHeaders } from '../enviroment/Authentication.js';
+import { baseUrl } from '.././enviroment/Header.js';
+import { getTokenResponse } from './Token.js';
+import { getCommonHeaders } from '.././enviroment/Header.js';
 
-export const options = {
-  stages: [
-    { duration: '1m', target: 100 },
-    { duration: '1m', target: 200 },
-    { duration: '30s', target: 0 }
-  ],
-
-  thresholds: {
-    http_req_duration: ['p(95)<1500'],  
-    http_req_failed: ['rate<0.01'],   
-  }
-};
+export const options = Configuration;
 
 export default function () {
-
-  const token = getTokenResponse();
-  const commonHeaders = getCommonHeaders(token);
-  
-  const employeeFilterPayload = {
-    "Type": true,
-    "FormName": null,
-    "View_Type": "0",
-    "Include_Resign": false,
-    "Include_Active": true,
-    "Include_Inactive": false,
-    "Resign_MonthOnly": false,
-    "OrderbyDept": true,
-    "OrderByDesign": false,
-    "Dval": "",
-    "Resign_CustomPeriod": false,
-    "Resign_DateRange": false,
-    "Fromdate": "2025-06-03",
-    "Todate": "2025-06-03",
-    "StartDate": null,
-    "EndDate": null,
-    "Exclude_Resign_DateRange": false,
-    "excludeStartDate": null,
-    "excludeEndDate": null,
-    "OrgSortbyOption": null
+  const bearerToken = getTokenResponse();
+  const GetEmployeeTreeViewSetupByViewMenuPayload = JSON.parse(open('./enviroment/GetEmployeeTreeViewSetupByViewMenuPayload.json'));
+  let headers = {
+    headers: getCommonHeaders(bearerToken)
   };
+  const FirstUrl = `${baseUrl}/employeesetupweb/GetEmployeeTreeViewSetupByViewMenu`;
+  
 
-  const res = http.post(`${baseUrl}/employeesetupweb/GetEmployeeTreeViewSetupByViewMenu`, JSON.stringify(employeeFilterPayload), {
-    headers: commonHeaders
-  });
+  const res = http.post(FirstUrl, GetEmployeeTreeViewSetupByViewMenuPayload,headers);
   check(res, {
     'TreeView status is 200': (r) => r.status === 200,
     'TreeView response time < 500ms': (r) => r.timings.duration < 500
